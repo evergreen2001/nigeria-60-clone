1aa895da64a907f95eb40ce3e87c050080a37213
unknown <dicksonevergreenido@gmail.com>
2020-09-07 08:21:24 +0100

    adding voting area

--- src/app/prizes/prizes.component.ts
+++ src/app/prizes/prizes.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-prizes',
   templateUrl: './prizes.component.html',
-  styleUrls: ['./prizes.component.css']
+  styleUrls: ['./prizes.component.css'],
 })
 export class PrizesComponent implements OnInit {
-
-  constructor() { }
+  public prizes;
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    this.prizes = [
+      {
+        price: '₦3,000,000',
+        position: 'THE WINNER',
+        award: 'Award Plaque',
+        certificate: 'Certificate of Accomplishment',
+      },
 
+      {
+        price: '₦1,500,000',
+        position: 'FIRST RUNNER UP',
+        award: 'Award Plaque',
+        certificate: 'Certificate of Accomplishment',
+      },
+
+      {
+        price: '₦1,000,000',
+
+        position: 'SECOND RUNNER UP',
+        award: 'Award Plaque',
+        certificate: 'Certificate of Accomplishment',
+      },
+
+
+    ];
+  }
 }
